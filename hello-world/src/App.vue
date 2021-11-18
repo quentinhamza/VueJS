be5e00c2a0f88f5935a9df05bd550bb27db5249c
quentinhamza <quentinhamzaent@gmail.com>
2021-11-18 16:41:15 +0100

    init

---
+++ hello-world/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <ul>
+    <li v-for="item in message" :key="item.message" id="tableau">{{ item }}</li>
+    </ul>
+
+    <button v-on:click="Afficher">CLICK</button>
+  
+  </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {    
+      return {
+       message: ['Mathis','Pierre','Romain','Souri','Quentin']
+      }
+    }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#tableau{
+  display: none;
+}
+</style>
